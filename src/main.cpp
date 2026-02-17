8b188614788b5b468a08e68c713e5a813251d4a4
thesignumt <tcrrxzz@gmail.com>
2026-02-17 18:54:14 +0000

    0.1.0-alpha.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <ostream>
+#include <string>
 
-int main() {
-  std::cout << "Hello, World!";
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    std::cout << "have args, argv[0]: " << argv[0] << std::endl;
+  } else {
+    std::string input;
+    while (true) {
+      std::cout << "> ";
+      std::getline(std::cin, input);
+
+      if (input == "exit") {
+        break;
+      }
+
+      std::cout << "got input";
+    }
+  }
   return 0;
 }
